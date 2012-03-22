9296421054d60560e7fb14d1662965c72dcf4b56
Renzo Lucioni <renzolucioni@gmail.com>
2012-03-21 22:48:27 -0400

    added basic implementation skeletons for LT and Tornado, added algorithms.ml to the README

--- algorithm.ml
+++ algorithm.ml
@@ -2,7 +2,7 @@
 module type FTN_CODE =
 sig
     (* kind of file being input *)
-    type file
+    type chunk
     
     (* encoding algorithm - can be specialized for different fountain codes *)
     val encode
@@ -12,4 +12,36 @@ sig
     
     (* transfer protocol - how the "fountain" is generated *)
     val gen_stream
+end
+
+(* LT structure *)
+module LTCode : (FTN_CODE with type chunk = TODO) =
+struct
+    (* LT chunk type *)
+    type chunk =
+
+    (* LT encoding function *)
+    let encode =
+
+    (* LT decoding function *)
+    let decode =
+
+    (* LT stream generation *)
+    let gen_stream =
+end
+
+(* Tornado structure *)
+module TornadoCode : (FTN_CODE with type chunk = TODO) =
+struct
+    (* Torndao chunk type *)
+    type chunk = 
+    
+    (* Tornado encoding function *)
+    let encode = 
+    
+    (* Tornado decoding function *)    
+    let decode = 
+
+    (* Tornado stream generation *)
+    let gen_stream =
 end
