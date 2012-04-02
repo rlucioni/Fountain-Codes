7279ca77133c0aaea280d1150c72d3b95689d2c2
Vipul Shekhawat <vish092@gmail.com>
2012-04-02 17:17:59 -0400

    updated fountain.ml and droplet.ml for tech spec

--- droplet.ml
+++ droplet.ml
@@ -1,18 +1,19 @@
 (* the Droplet, containing a seed and the XOR'd data *)
-class type droplet =
+class type droplet data total seed =
 object
-    (*A few chunks of the file encoded togehter*)
+    (* a few chunks of the file encoded together *)
     val mutable data
     
-    (* each individual chunk's number - inefficient*)
-    val mutable how_many
-    
-    (* Total number of chunks in file *)
+    (* total number of chunks in file *)
     val mutable total_pieces
     
     (* pseduorandomly generated seed for telling which chunks were used *)
     val mutable seed
     
-    (*This prints the data, num_chunks, seed *)
+    (* this prints the data, total_pieces, and seed *)
     method to_string: unit
+
+    (* returns a tuple with data, total_pieces, and seed for use in
+       goblet *)
+    method get_contents
 end
--- fountain.ml
+++ fountain.ml
@@ -1,17 +1,41 @@
-(* the Fountain, produces Droplets according to the fountain code 
+open Droplet
+
+(* the fountain produces droplets according to the fountain code 
  * implementation chosen *)
-class type fountain =
+class type fountain data size =
 object
-    (* data to be packed up *)
+  
+    (* the total data (file) being transferred *)
     val mutable data
-    
+
+    (* the size of each piece of data from the file, eg 16 bytes *)
+    val mutable piece_size
+
+    (* total number of pieces in the original file *)
+    val mutable total_pieces
+
+    (* seed generated using a PRNG, to determine which pieces
+       of the file we're using and how many pieces are in
+       each XOR'd droplet *)
     val mutable seed
     
-    (* within this chunk_size and num_chunks *)
-    val mutable chunk
+    (* for the generated droplet, how many pieces of the
+       original file are we using to XOR? *)
+    val mutable how_many
     
-    val mutable droplet
-    
-    (*This updates the seed so we can decode droplets*)
-    method update_seed 
+    (* this creates a new random seed to generate new droplets *)
+    method random_seed
+
+    (* this uses the seed to come up with a new random number of
+       pieces being encoded in the next droplet *)
+    method random_howmany
+
+    (* this uses the seed and data variables to fetch a random
+       piece of the original file *)
+    method get_piece
+
+    (* this generates a new random droplet object, using the above 
+       methods and instance variables *)
+    method output_droplet
+
 end
