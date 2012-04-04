0b1535f149c61198d4603f0acaa6d3773e367ce1
Daniel Broudy <broudy@college.harvard.edu>
2012-04-04 17:33:42 -0400

    some work on goblet

--- goblet.ml
+++ goblet.ml
@@ -1,20 +1,26 @@
 open Droplet
 
+ (* This is a type for metadrop which is a droplet that had its seed decoded into metadata
+  * number_chunks is how many chunks went into the droplet
+  * pieces_list is the chunks that went into it
+  * data is obvious   *)
+type metadrop = { number_chunks : int ; pieces_list : int list ; data : string  }
+
 (* a Goblet, used to collect Droplets and reconstruct the original data *)
 class type goblet =
 object
     (* total number of pieces in the original file *)
-    val mutable total_pieces
+    val mutable total_pieces : int
     
     (* data structure representing the part of the message we have decoded so
      * far *)
     val mutable message
 
     (* number that shows how much of the file we have decoded, in pieces *)
-    val mutable counter
+    val mutable counter : int
 
-    (* this function generates a new droplet from the stream of droplets*)
-    method get_droplet: unit
+    (* takes the droplet d  and returns the metadrop  *)
+    method get_metadrop: metadrop
 
     (* takes a droplet as an argument, decodes if possible, updating message
      * with decoded content if applicable *)
