3d77a29702f46369ec9d8a11444fb97b4f8f19c2
Renzo Lucioni <renzolucioni@gmail.com>
2012-04-03 20:12:06 -0400

    added concrete class to droplet.ml

--- droplet.ml
+++ droplet.ml
@@ -1,19 +1,36 @@
+open Printf
+
 (* the Droplet, containing a seed and the XOR'd data *)
-class type droplet data total seed =
+class type droplet (d: int list) (t: int) (s: int) =
 object
     (* a few pieces of the file encoded together *)
-    val mutable data
-    
+    val mutable data : int list
+             
     (* total number of pieces in file *)
-    val mutable total_pieces
-    
+    val mutable total_pieces : int
+                 
     (* pseduorandomly generated seed for telling which pieces were used *)
-    val mutable seed
-    
-    (* this prints the data, total_pieces, and seed *)
-    method to_string: unit
+    val mutable seed : int
+                         
+    (* this converts the data, total_pieces, and seed into a string *)
+    method to_string : string
+                                 
+    (* returns a tuple with data, total_pieces, and seed for use in goblet *)
+    method get_contents : int list * int * int
+end
 
-    (* returns a tuple with data, total_pieces, and seed for use in
-       goblet *)
-    method get_contents
+
+class LTdroplet : droplet (d: int list) (t: int) (s: int) =
+object
+    val mutable data = d
+    val mutable total_pieces = t
+    val mutable seed = s
+                        
+    (* still need to implement printing for testing *)
+    method to_string : string =
+        "{Data :" ^ (String.concat "" (List.map (string_of_int data))) ^ 
+        ", Total pieces: " ^ (string_of_int total_pieces) ^ ", Seed: " ^
+        (string_of_int seed) ^ "}"
+                                                                    
+    method get_contents = (data, total_pieces, seed)
 end
