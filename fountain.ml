13ba6d90d133d3031d30455f9b1a6b8d55fd00db
Vipul Shekhawat <vish092@gmail.com>
2012-04-04 11:43:08 -0400

    removed comment

--- fountain.ml
+++ fountain.ml
@@ -58,7 +58,6 @@ object (this)
     
     method get_piece         = int_of_char data.[int total_pieces]
 
-    (* causes stack overflow for some reason, still needs to be debugged *)
     method xor               =
       let rec help_xor (n:int) : int =
 	if n > 0 then
