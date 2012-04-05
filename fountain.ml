5243c5f667619d6c0b8dd90d133178f46fa074d4
Daniel Broudy <broudy@college.harvard.edu>
2012-04-05 16:46:14 -0400

    raised a question in fountain.ml i think something was called twice erroneously

--- fountain.ml
+++ fountain.ml
@@ -57,7 +57,7 @@ object (this)
     
     method get_piece           = int_of_char data.[int total_pieces]
 
-    method xor                 = this#random_seed; this#rand_droplet_pieces;
+    method xor                 =(* this#random_seed; this#rand_droplet_pieces; we think this isn't needed *)
         let rec help_xor (n:int) : int =
 	        if (n > 1) 
                 then ((lxor) (this#get_piece) (help_xor (n-1)))
