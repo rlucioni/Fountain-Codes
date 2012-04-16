4dd69f3e1516fbb5cdf581a9c485e351830e13f0
Renzo Lucioni <renzolucioni@gmail.com>
2012-04-15 23:04:15 -0400

    changed the test message to something more reasonable

--- operations.ml
+++ operations.ml
@@ -4,23 +4,7 @@ open Droplet
 open Fountain
 open Goblet
 
-let message = "Checkpoint 1 Write Up
-Project name: Fountain Codes
-Group member names and emails: 
-    Renzo Lucioni: rlucioni@college.harvard.edu
-    Vipul Shekhawat: vshekhawat@college.harvard.edu
-    Daniel Broudy: daniel.broudy@gmail.com
-    Peregrine Badger: pbadger@college.harvard.edu
-    Status Report
-    Progress
-    Over the past week, we have met all of the goals for the week we set out for
-    ourselves on the timeline last week. We decided to break down an input
-    message into 1-character pieces for the alpha. We cleaned up unnecessary
-    code in Fountain and rewrote the format in Droplets store information -
-    formerly, it was a list, now it is a record. The majority of the week was
-    spent implementing Goblet, which was nonexistent last week. With these
-    changes, we have attained basic functionality. Here’s how it works."
-
+let message = "this is a test!"
 
 let f = new lt_fountain message 1 10
 let g = new lt_goblet f#output_droplet 10
