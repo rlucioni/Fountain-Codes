f58b8655384471fa121fa5ae7479f0ff410aaf2a
Renzo Lucioni <renzolucioni@gmail.com>
2012-04-15 23:01:16 -0400

    finished script in operations

--- operations.ml
+++ operations.ml
@@ -4,19 +4,34 @@ open Droplet
 open Fountain
 open Goblet
 
-let transmit (message : string) : unit = 
-    let f = new lt_fountain message 1 5 in
-    let g = new lt_goblet f#output_droplet 5 in
-    let rec helper () = 
-        if g#check_complete
-          then ()
-          else (g#get_droplet f#output_droplet); 
-          g#decode; g#print_progress;
-          helper ()
-    in
-    helper ()
+let message = "Checkpoint 1 Write Up
+Project name: Fountain Codes
+Group member names and emails: 
+    Renzo Lucioni: rlucioni@college.harvard.edu
+    Vipul Shekhawat: vshekhawat@college.harvard.edu
+    Daniel Broudy: daniel.broudy@gmail.com
+    Peregrine Badger: pbadger@college.harvard.edu
+    Status Report
+    Progress
+    Over the past week, we have met all of the goals for the week we set out for
+    ourselves on the timeline last week. We decided to break down an input
+    message into 1-character pieces for the alpha. We cleaned up unnecessary
+    code in Fountain and rewrote the format in Droplets store information -
+    formerly, it was a list, now it is a record. The majority of the week was
+    spent implementing Goblet, which was nonexistent last week. With these
+    changes, we have attained basic functionality. Here’s how it works."
 
-transmit "this is an awesome test" ;;
+
+let f = new lt_fountain message 1 10
+let g = new lt_goblet f#output_droplet 10
+
+let rec transmit () : unit = 
+    if g#check_complete
+      then g#print_progress
+      else ((g#get_droplet f#output_droplet); 
+           g#decode; ignore(g#get_message); transmit ()) ;;
+
+transmit () ;;
 
 (* sample usage of the testing framework *)
 (*
