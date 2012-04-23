5900c063dac5f5d8f79f12cabf56110ca4dbecfd
Renzo Lucioni <renzolucioni@gmail.com>
2012-04-22 20:05:09 -0400

    added string_droplet.ml

---
+++ string_droplet.ml
@@ -0,0 +1,37 @@
+type droplet_record = {data : char; total_pieces : int; seed : int}
+
+(* the Droplet, containing a seed and the XOR'd data *)
+class type droplet =
+object
+    (* a few pieces of the file encoded together *)
+    val mutable data : char
+             
+    (* total number of pieces in file *)
+    val mutable total_pieces : int
+                 
+    (* pseduorandomly generated seed for telling which pieces were used *)
+    val mutable seed : int
+                         
+    (* this converts the data, total_pieces, and seed into a string *)
+    method to_string : string
+                                 
+    (* returns record with data, total_pieces, and seed for use in goblet *)
+    method get_contents : droplet_record
+end
+
+
+class lt_droplet (d: char) (t: int) (s: int) : droplet =
+object
+    val mutable data         = d
+    val mutable total_pieces = t
+    val mutable seed         = s
+                        
+    (* still need to implement printing for testing *)
+    method to_string : string =
+        "{Data :" ^ (Char.escaped data)
+(*(String.concat "" (List.map (string_of_int data)))*) ^ 
+        ", Total pieces: " ^ (string_of_int total_pieces) ^ ", Seed: " ^
+        (string_of_int seed) ^ "}"
+                                                                    
+    method get_contents = {data = data; total_pieces = total_pieces; seed = seed}
+end
