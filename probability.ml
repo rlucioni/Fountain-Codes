077149ba1e48c4e146f721aa2aa922dac8d4f586
Renzo Lucioni <renzolucioni@gmail.com>
2012-04-29 15:30:20 -0400

    fixed probability to work with dropping packets, although now it only seems to run transmit once and print the same result num times to run times

--- probability.ml
+++ probability.ml
@@ -30,11 +30,19 @@ match distro with
     new normal_fountain m v message piece_size 1)
   | _ -> failwith "Not a valid distribution." ;;
 
+
+(* gets and then strips the option from a droplet *)
+let rec get_droplet () : droplet  = 
+  let a = f#output_droplet in
+  match a with
+  |None   -> get_droplet ()
+  |Some d -> d
+
 let g =
 match distro with
-  | "unif" -> new lt_goblet f#output_droplet f#get_bound
-  | "pois" -> new poisson_goblet f#get_mean f#output_droplet 1
-  | "norm" -> new normal_goblet f#get_mean f#get_var f#output_droplet 1
+  | "unif" -> new lt_goblet (get_droplet ()) f#get_bound
+  | "pois" -> new poisson_goblet f#get_mean (get_droplet ()) 1
+  | "norm" -> new normal_goblet f#get_mean f#get_var (get_droplet ()) 1
   | _ -> failwith "Not a valid distribution."
 
 let n = Printf.printf "# times to run: "; read_int() ;;
@@ -48,11 +56,14 @@ let rec transmit () : int =
            transmit ()) ;;
 
 let rec tester lst n : int list =
-  if n = 0 then lst else tester (transmit()::lst) (n-1)
+  if n = 0 
+    then lst 
+    else tester ((transmit ()) :: lst) (n-1)
 
 let rec stringify lst : string =
   match lst with
     | [] -> ""
-    | hd::tl -> string_of_int hd ^ "," ^ (stringify tl) ;;
+    | hd::tl -> string_of_int hd ^ "," ^ (stringify tl);;
 
-print_string (stringify (tester [] n))
+print_string (stringify (tester [] n));
+print_string "\n"
